e4c655105bae6976b14ccefb12a54a508ef9ae2b
Jorge Soto <jorgesoto@Jorges-MacBook-Pro.local>
2019-03-20 20:47:06 -0700

    Creacion de paginas de los juegos

--- proyecto/aplicacion/urls.py
+++ proyecto/aplicacion/urls.py
@@ -9,5 +9,12 @@ urlpatterns = [
     path('informacionUsuario/<int:pk>', views.InformacionUsuario.as_view(), name="InformacionUsuario"),
     path('EditarUsuario/<int:pk>', views.EditarUsuario.as_view(), name="EditarUsuario"),
     path('BajaUsuario/<int:pk>', views.EliminarUsuario, name="EliminarUsuario"),
+
+    #Urls de juegos
     path('menu/JuegosMatematicas/', views.JuegosMatematicas, name='JuegosMatematicas'),
+    path('menu/JuegosEspanol/', views.JuegosEspanol, name='JuegosEspanol'),
+    path('menu/JuegosCiencias/', views.JuegosCiencias, name='JuegosCiencias'),
+    path('menu/JuegosHistoria/', views.JuegosHistoria, name='JuegosHistoria'),
+    path('menu/JuegosCivismo/', views.JuegosCivismo, name='JuegosCivismo'),
+    path('menu/JuegosGeografia/', views.JuegosGeografia, name='JuegosGeografia'),
 ]
